5cce240815fbb974b0140d994798b7308cd906d6
Christina Merlin <81285621+Christinamerlin@users.noreply.github.com>
2023-06-04 22:39:10 +0530

    Create prime num.c

---
+++ prime num.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,c,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        if(n%i==0){
+            c++;
+        }
+    }
+    if(c<=2){
+        printf("PRIME");
+    }
+    else{
+        printf("NOT PRIME");
+    }
+    return 0;
+}
