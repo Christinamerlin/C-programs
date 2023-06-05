e24c79ebe3a5024969fcc31dec2a72e10f84b819
Christina Merlin <81285621+Christinamerlin@users.noreply.github.com>
2023-06-05 22:36:56 +0530

    pattern1.c

---
+++ pattern1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){     
+        for(j=i;j>=1;j--){  
+            printf("%d ",i); 
+        }
+        printf("\n");
+    }
+     
+}
+/* 
+Input Format
+5
+
+Constraints
+
+zero
+
+Output Format
+
+5 5 5 5 5
+4 4 4 4 
+3 3 3
+2 2 
+1
+
+
+
+*/
